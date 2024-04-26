6de2af1d1ad8c99e19bf8a11c0705f992b5dcfa3
Hellicat <jhccop@naver.com>
2024-04-27 01:54:16 +0900

    📝 Edit Route path & use States
    edit Route path & use States

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,27 @@ import {dark, light} from "./libs/theme.ts";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Preview from "./page/beforeLogin/preview/preview.tsx";
 import Home from "./page/afterLogin/home/home.tsx";
-import {useAppSelector} from "./hooks/storeHook.ts";
-import {useEffect} from "react";
+
 import Login from "./page/beforeLogin/login/login.tsx";
 import Join from "./page/beforeLogin/join/join.tsx";
+import {useAppSelector} from "./hooks/storeHook.ts";
+import {urlName} from "./libs/constants.ts";
 
 function App() {
-  const token = useAppSelector((state) => state.token.token);
-  useEffect(() => {}, [token]);
+  const {token} = useAppSelector((state) => state.token);
+  const {theme} = useAppSelector((state) => state.theme);
   return (
-    <ThemeProvider theme={light}>
+    <ThemeProvider theme={!theme ? light : dark}>
       <GlobalStlye />
       <Layout>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={!token ? <Preview /> : <Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/join" element={<Join />} />
+            <Route
+              path={urlName.home}
+              element={!token ? <Preview /> : <Home />}
+            />
+            <Route path={urlName.login} element={<Login />} />
+            <Route path={urlName.join} element={<Join />} />
           </Routes>
         </BrowserRouter>
       </Layout>
