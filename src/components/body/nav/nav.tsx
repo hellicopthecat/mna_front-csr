0bdd1220f3b1193946cbf5ad0a507bba963ab198
Hellicat <jhccop@naver.com>
2024-05-01 14:55:54 +0900

    💄 Edit nav style
    edit nav style

--- src/components/body/nav/nav.tsx
+++ src/components/body/nav/nav.tsx
@@ -3,16 +3,31 @@ import {useAppDispatch, useAppSelector} from "../../../hooks/storeHook";
 import {nonActiveNav} from "../../../redux/navSlice";
 
 const NavWrapper = styled.nav<{$active: boolean}>`
+  position: absolute;
+  width: 100%;
   display: flex;
   flex-direction: column;
-  background-color: ${(props) => props.theme.secondary};
-  width: 30%;
+  background-color: rgba(0, 0, 0, 0.4);
   height: 100dvh;
-  padding: 20px;
-  position: absolute;
   transform: translateX(${(props) => (props.$active ? "0" : "-100%")});
-  transition: 0.4s ease-in-out;
+  transition: 0.2s ease-in-out;
   z-index: 99;
+  div {
+    background-color: ${(props) => props.theme.secondary};
+    width: 30%;
+    height: 100%;
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+  }
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    font-size: 20px;
+    font-weight: 600;
+  }
 `;
 const NavIcon = styled.svg`
   width: 25px;
@@ -23,29 +38,30 @@ const NavIcon = styled.svg`
 const NavComp = () => {
   const {clickNav} = useAppSelector((state) => state.activeNav);
   const dispatch = useAppDispatch();
-
   return (
     <NavWrapper $active={clickNav}>
-      <NavIcon
-        fill="none"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true"
-        onClick={() => dispatch(nonActiveNav())}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-        />
-      </NavIcon>
-      <ul>
-        <li>회사</li>
-        <li>제품</li>
-        <li>수입 / 지출</li>
-      </ul>
+      <div>
+        <NavIcon
+          fill="none"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          onClick={() => dispatch(nonActiveNav())}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </NavIcon>
+        <ul>
+          <li>회사</li>
+          <li>제품</li>
+          <li>수입 / 지출</li>
+        </ul>
+      </div>
     </NavWrapper>
   );
 };
